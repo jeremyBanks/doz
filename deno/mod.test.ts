3b4b534efb0c01efb939f00f06c35711e685f2be
Jeremy Banks <_@jeremy.ca>
2020-12-03 16:53:55 -0500

    ⚙️ rebuild 358245fb (test the deno reexport)
    https://github.com/jeremyBanks/doz/actions/runs/399377598

--- deno/mod.test.ts
+++ deno/mod.test.ts
@@ -1,10 +1,10 @@
-import defaultZod from "./mod.ts";
-import * as starZod from "./mod.ts";
+import defaultZod from './mod.ts';
+import * as starZod from './mod.ts';
 
-Deno.test("zod/deno/mod.ts members and types imported from default", () => {
+Deno.test('zod/deno/mod.ts members and types imported from default', () => {
   const _: defaultZod.ZodTypeAny = defaultZod.record(defaultZod.bigint());
 });
 
-Deno.test("zod/deno/mod.ts members and types imported from *", () => {
+Deno.test('zod/deno/mod.ts members and types imported from *', () => {
   const _: starZod.ZodTypeAny = starZod.record(starZod.bigint());
 });
